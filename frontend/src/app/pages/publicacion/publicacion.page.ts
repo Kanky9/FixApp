124db32f201b67799a9335b8e56a1bdd1cc82755
Kanky9 <bournotsantino03@gmail.com>
2024-06-14 14:10:32 -0300

    Botón a google maps desde la publicación, 14-06-2024

--- frontend/src/app/pages/publicacion/publicacion.page.ts
+++ frontend/src/app/pages/publicacion/publicacion.page.ts
@@ -1,6 +1,7 @@
 import { Component, inject, Input, OnInit, SecurityContext } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { Platform } from '@ionic/angular';
 import { Product } from 'src/app/models/product.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
@@ -11,7 +12,7 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 })
 export class PublicacionPage implements OnInit {
 
-   publicacionId: string;
+  publicacionId: string;
   publicacion: Product;
   image: string[] = [];
   selectedIndex = 0;
@@ -21,6 +22,7 @@ export class PublicacionPage implements OnInit {
   route = inject(ActivatedRoute);
   firebaseSvc = inject(FirebaseService);
   sanitizer = inject(DomSanitizer);
+  plataform = inject(Platform); 
 
   ngOnInit() {
     this.publicacionId = this.route.snapshot.paramMap.get('productId');
@@ -52,5 +54,17 @@ export class PublicacionPage implements OnInit {
   getSanitizedHtml(text: string) {
     const transformedText = this.transformNewLinesToBr(text);
     return this.sanitizer.sanitize(SecurityContext.HTML, transformedText);
+  }
+
+  abrirEnGoogleMaps(ubicacion: string, direccion: string ) {
+    const isMobile = this.plataform.is('cordova') || this.plataform.is('capacitor'); 
+    const query = encodeURIComponent(`${ubicacion}, ${direccion}`); 
+    const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${query}`; 
+
+    if (isMobile) {
+      window.open(`geo:0,0?q=${query}`, '_system'); 
+    } else {
+      window.open(googleMapsUrl, '_blank');
+    }
   }
 }
